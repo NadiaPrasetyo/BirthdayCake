efdd298932327b5a553c15e8f30e268cd459d4c9
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-08-14 17:19:07 +1200

    Card style and pop up

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function process (form) {
-    var nameText = form.name.value;
-    var dobText = form.birthday.value;
+function process () {
+    var nameText = document.getElementById("name").value;
+    var dobText = document.getElementById("birthday").value;
     var e = document.getElementById("flavour");
     var flavourText = e.options[e.selectedIndex].text;
-    var messageText = form.message.value;
-    alert ("Name: " + nameText + "\nBirthday: " + dobText + "\nFlavour: " + flavourText + "\nMessage: " + messageText);
+    var messageText = document.getElementById("message").value;
+    // alert ("Name: " + nameText + "\nBirthday: " + dobText + "\nFlavour: " + flavourText + "\nMessage: " + messageText);
 
+    let card = document.getElementById("Card");
+    card.style.visibility = "hidden";
+
+    // make the form not restart the page
+    event.preventDefault();
+    
 }
