04d7116fd86d85434e90e62cc6676a11f180f402
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-08-14 20:26:01 +1200

    Card style changes

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function validname(nameText) {
         document.getElementById("name").style.border = "1px solid red";
         return false;
     }
-    document.getElementById("name").style.border = "1px solid black";
+    document.getElementById("name").style.border = "1px solid hsl(266, 59%, 60%)";
     return true;
 }
 
@@ -107,7 +107,7 @@ function validbirthday(dobText) {
         document.getElementById("dob").style.border = "1px solid red";
         return false;
     }
-    document.getElementById("dob").style.border = "1px solid black";
+    document.getElementById("dob").style.border = "1px solid hsl(266, 59%, 60%)";
     return true;
 }
 
