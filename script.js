4a4ed7129de44caf7b93d0cbbe6bb28567752729
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-08-16 22:36:58 +1200

    draggable ribbon and message card made

--- script.js
+++ script.js
@@ -567,6 +567,9 @@ function stop_microphone() {
     }
 }
 
+/*After the blow ---------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------*/
 
 /**
  * Function for the remove candles button
@@ -588,4 +591,69 @@ function removeCandles() {
     // Add the addCandle event listener to the cake
     // From now on the user can add candles to the cake by clicking on it
     cake.addEventListener("click", addCandle);
+}
+
+/**
+ * Function to make the message card appear
+ */
+function messageCardAppear(){
+
+}
+
+/* Drag ribbon ---------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------*/
+
+// Make the DIV element draggable:
+document.addEventListener("DOMContentLoaded", function() {
+    dragElement(document.getElementById("ribbon"));
+});
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos3 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos3 = e.clientX;
+    // set the element's new position:
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+    // make a maximum and minimum position for the ribbon
+    // min is -25vw, max is 0vw
+    let vw = window.innerWidth / 100;
+    if (elmnt.offsetLeft < (-25 * vw)) {
+        elmnt.style.left = "-25vw";
+    }
+    if (elmnt.offsetLeft > 0) {
+        elmnt.style.left = "0vw";
+        console.log("max");
+    }
+    
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
